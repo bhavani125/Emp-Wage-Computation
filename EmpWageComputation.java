fd558eddfaa8e4ca04262daedc14219b6f6e69c1
bhavani125 <girinenibhavani12@gmil.com>
2021-06-16 07:18:47 +0530

    added code of daily employe wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,12 +3,21 @@ public class EmpWageComputation
   public static void main(String[] args)
   {
      int isFullTime=1;
+     int DailyWage;
+     int WagePerHr=20;
+     int WorkingHrs;
      double empcheck = Math.floor(Math.random() * 10) % 2;
      if(empcheck == isFullTime)
      {
        System.out.println("Employee is present");
+       WorkingHrs=8;
      }
      else
+      {
         System.out.println("Employee is Absent");
-   }
+        WorkingHrs=0;
+      }
+     DailyWage=WorkingHrs * WagePerHr;
+     System.out.println("Daily wage of employee is " + DailyWage);
+    }
 }
